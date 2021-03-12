e1622941e63d11680558b328f0bb17803c13cbfa
sct <sctsnipe@gmail.com>
2021-03-12 10:17:56 +0000

    feat: test publish

---
+++ release.config.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
